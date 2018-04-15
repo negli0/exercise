ea971fc206cb931c77fe3658ded1e989a0078d68
Hiroki Watanabe <nelio@inl.ics.keio.ac.jp>
2018-04-16 00:58:17 +0900

    comp

--- csacademy/odd-sum.c
+++ csacademy/odd-sum.c
@@ -1,57 +1,36 @@
 #include <stdio.h>
 
-void print_array(char *str, int *array, int num);
-
 int main() {
-    int odd_array[51] = {0};
-    int even_array[51] = {0};
+    int num_array[101] = {0};
     int N;
-    int max = 0;
-    int min = 100;
     int num;
     scanf("%d", &N);
-    int num_count = 0;
-	
-    for (int i = 0; i < N; i++) {
+
+    for (int i = 1; i <= N; i++) {
         scanf("%d", &num);
-        if (num % 2 == 0) {
-            even_array[num / 2] = 1;
-        } else {
-            odd_array[num / 2] = 1;
-        }
+        num_array[i] = num;
     }
 
     int answer = 0;
+    int last = 0;
+    int last_count = 0;
 
-    for (int i = min; i <= max; i++) {
-        if (i % 2 == 0) {
-            for (int j = i / 2 + 1; j < 50; j++) {
-                if (odd_array[j] == 1) {
-                   answer++; 
-                }
-            }
-        } else {
-            for (int j = i / 2 + 2; j <= 50; j++) {
-                if (even_array[j] == 1) {
-                   answer++; 
-                }
+    for (int i = 1; i <= N; i++) {
+        if (num_array[i] == last) {
+            answer += last_count;
+            continue;
+        }
+
+        last_count = 0;
+
+        for (int j = i + 1; j <= N; j++) {
+            if ((num_array[i] + num_array[j]) % 2 != 0) {
+                answer++;
+                last_count++;
             }
         }
+        last = num_array[i];
     }
-
-    print_array("odd", odd_array, 50);
-	print_array("even", even_array, 50);   
     printf("%d", answer);
-
     return 0;
-
-}
-
-
-
-void print_array(char *str, int *array, int num)
-{
-    for (int i = 0; i < num; i++) {
-        printf("%s_array[%d] = %d\n", str, i, array[i]);
-    }
 }
